73626fbce6a442a8e8837c7d868adf570a59cdde
Kieron Holmes <kieron@kbho.co.uk>
2021-10-07 18:15:53 +0100

    Initial Code Commit (Django)

---
+++ documents/models.py
@@ -0,0 +1,11 @@
+import uuid
+from django.db import models
+
+class Documents(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey('authentication.CustomUser', related_name="documents", on_delete=models.CASCADE)    
+    
+    class Meta:
+        ordering = ['-updated_at']
