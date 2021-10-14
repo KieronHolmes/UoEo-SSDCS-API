f89fab3c5d325ec9a1274df41389dcdde6e44cff
KieronHolmes <kieron@kbho.co.uk>
2021-10-14 17:47:10 +0100

    Added brief documentation for each fetchable method (For the Swagger Docs). Mock tests added for token fetching.

--- authentication/views.py
+++ authentication/views.py
@@ -6,6 +6,9 @@ from .serializers import RegisterSerializer
 
 
 class RegisterView(CreateAPIView):
+    """
+    Provide the functionality for a user to register for a new account.
+    """
     queryset = CustomUser.objects.all()
     permission_classes = (permissions.AllowAny,)
     serializer_class = RegisterSerializer
