733f07c01830950c3b9387a0991ccb1cffd2bcbe
KieronHolmes <kieron@kbho.co.uk>
2021-10-18 22:16:49 +0100

    Implemented Subject Erasure Requests

--- gdpr/urls.py
+++ gdpr/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SubjectAccessRequestView
+from .views import SubjectAccessRequestView, SubjectErasureRequestView
 
 urlpatterns = [
     path("access-request/", SubjectAccessRequestView.as_view({'get': 'list'}), name="subject_access_request"),
+    path("erasure-request/", SubjectErasureRequestView.as_view(), name="subject_access_request"),
 ]
--- gdpr/views.py
+++ gdpr/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from authentication.models import CustomUser
 from .serializers import GDPRSerializer
@@ -8,13 +10,34 @@ from .serializers import GDPRSerializer
 
 class SubjectAccessRequestView(ReadOnlyModelViewSet):
     """
-    A function to retrieve all data held about the currently logged in user.
+    list:
+        x
     """
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = GDPRSerializer
     pagination_class = None
 
+    @extend_schema(
+        summary="Performs a Subject Access Request.",
+        description="Performs a Subject Access Request with the currently logged in users details.",
+        tags=["GDPR"]
+    )
     def list(self, request, *args, **kwargs):
-        instance = request.user
-        serializer = self.get_serializer(instance)
+        user = request.user
+        serializer = self.get_serializer(user)
         return Response(serializer.data)
+
+
+class SubjectErasureRequestView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    @extend_schema(
+        summary="Performs a Subject Erasure Request",
+        description="Deletes all the information held on the currently logged in user (Including User Account).",
+        tags=["GDPR"]
+    )
+    def delete(self, request, *args, **kwaargs):
+        user = request.user
+        user.delete()
+
+        return Response({"result": "Subject Erasure Request Completed."})
