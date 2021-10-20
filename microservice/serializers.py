727f38314e33c6b8c0042d817419225be321e667
KieronHolmes <kieron@kbho.co.uk>
2021-10-20 18:50:21 +0100

    Query URL returned. URL naming added. Base tests created.

--- microservice/serializers.py
+++ microservice/serializers.py
@@ -13,8 +13,13 @@ class ItemSerializer(serializers.Serializer):
 
 class ResultSerializer(serializers.Serializer):
     query = serializers.CharField()
+    query_url = serializers.SerializerMethodField()
     count = serializers.SerializerMethodField()
     results = ItemSerializer(many=True)
 
+    def get_query_url(self,obj):
+        return f"https://cds.cern.ch/search?p={obj['query']}&of=recjson&ot=title,authors,creation_date"
+
     def get_count(self, obj):
         return len(obj['results'])
+
--- microservice/urls.py
+++ microservice/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import MicroserviceSearchView
 
 urlpatterns = [
-    path('<str:query>', MicroserviceSearchView.as_view())
+    path('<str:query>', MicroserviceSearchView.as_view(), name="microservice_search")
 ]
