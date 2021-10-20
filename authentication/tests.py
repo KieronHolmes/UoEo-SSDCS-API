3882176dcfc30402f4cba83db56f3a5791191474
Restyled.io <commits@restyled.io>
2021-10-20 19:08:27 +0000

    Restyled by black

--- authentication/tests.py
+++ authentication/tests.py
@@ -31,7 +31,7 @@ class RegisterTests(APITestCase):
             "username": "invalid-user",
             "email": "ThisIsNotAnEmailAddress",
             "password": "StR0nGP45SWorD",
-            "role": "admin"
+            "role": "admin",
         }
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
