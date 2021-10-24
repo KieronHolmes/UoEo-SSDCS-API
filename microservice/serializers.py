82eb0215020251c1228d665e49b08fba8f8be50e
Restyled.io <commits@restyled.io>
2021-10-24 20:39:15 +0000

    Restyled by isort

--- microservice/serializers.py
+++ microservice/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework import serializers
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
+from rest_framework import serializers
 
 
 class AuthorSerializer(serializers.Serializer):
